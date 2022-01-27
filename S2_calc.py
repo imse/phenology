17742272aec89a0667671805039dff95aeb1aab8
iserra <i.serra@.creaf.uab.cat>
2022-01-27 12:07:47 +0100

    changes python

--- S2_calc.py
+++ S2_calc.py
@@ -11,7 +11,7 @@ import xarray as xr
 import pandas as pd
 import rioxarray
 import numpy as np
-
+import pyproj
 ##
 from pathlib import Path
 from glob import glob
@@ -141,75 +141,6 @@ def affine(b):
     return lon,lat
 
 
-# connect to the API
-"""
-from sentinelsat import SentinelAPI, read_geojson, geojson_to_wkt
-from datetime import date
-api = SentinelAPI('', '', 'https://scihub.copernicus.eu/dhus')
-"""
-
-"""
-fileBbox = sys.argv[1]   #geojosn file closed linestring
-fileTemporal = sys.argv[2]   #txt file
-
-
-with open(fileTemporal) as f:
-#with open('input/dates.txt') as f:
-    contents = f.read()
-    dates = contents.split(",")
-    print(dates)
-
-footprint = geojson_to_wkt(read_geojson(fileBbox))
-products = api.query(footprint,
-                     #date = ('20190625', date(2019, 6, 25)),
-                     #date = ('20190625','20190630'),
-                     #date = (fileTemporal, file3),
-                     date = (dates[0],dates[1]),
-                     platformname = 'Sentinel-2',
-                     processinglevel = 'Level-2A',
-                     cloudcoverpercentage = (0, 80))  #80%
-
-
-print(len(products))
-
-for i in products:
-    print (i)
-
-# convert to Pandas DataFrame
-products_df = api.to_dataframe(products)
-# sort and limit to first 1 sorted products
-products_df_sorted = products_df.sort_values(['cloudcoverpercentage', 'ingestiondate'], ascending=[True, True])
-products_df_sorted = products_df_sorted.head(20)
-
-if not os.path.exists('temp'):
-    os.makedirs('temp')
-
-download_path = './temp'
-api.download_all(products_df_sorted.index,directory_path=download_path)
-
-download_unzipped_path = os.path.join(os.getcwd(), 'unzipped')  #unzipped
-#print(download_unzipped_path)
-
-extension = ".zip"
-for item in os.listdir(download_path):
-    print(item)
-    if item.endswith(extension): # check for ".zip" extension
-        file_name = os.path.join(download_path, item) # get full path of files
-        zip_ref = zipfile.ZipFile(file_name) # create zipfile object
-        zip_ref.extractall(download_unzipped_path) # extract file to dir
-        zip_ref.close() # close file
-
-
-files = os.listdir(download_unzipped_path)
-Part2 of S2_ndviF.py
-"""
-"""
-mask_lon = (ds.x >= 417441) & (ds.x <= 429770)   #   429770.327 4666780.373
-mask_lat = (ds.y >= 4666780) & (ds.y <= 4672466)  #417441.799 4672466.355
-cropped_ds = ds.where(mask_lon & mask_lat, drop=True)
-
-"""
-
 #def phen:
 
 ARG=json.load(open("vlabparams.json","r"))
@@ -218,31 +149,29 @@ print(str(ARG['bbox'][0])) #1
 print(str(ARG['bbox'].split(","))) #['19.1031', ' 64.0410', ' 19.8981', '64.4038']
 
 print(str(ARG['bbox'])) #19.1031, 64.0410, 19.8981,64.4038
-xmin=ARG['bbox'].split(",")[0]
-ymin=ARG['bbox'].split(",")[1]
+lon_min=ARG['bbox'].split(",")[0]
+lat_min=ARG['bbox'].split(",")[1]
+lon_man=ARG['bbox'].split(",")[2]
+lat_man=ARG['bbox'].split(",")[3]
 
 
-print(str(xmin))
-print(ymin)
+print(str(lon_min))
+print(lat_min)
+print(lon_man)
 
-#txt = '19.1031, 64.0410, 19.8981, 64.4038'
-#x = txt.split(", ")[1]
-#print(x)
+proj_wgs84 = pyproj.Proj(init="epsg:4326")
+proj_gk4 = pyproj.Proj(init="epsg:32632")   #EPSG:32632
+x_min, y_min = pyproj.transform(proj_wgs84, proj_gk4, lon_min, lat_min)
+x_max, y_max = pyproj.transform(proj_wgs84, proj_gk4, lon_max, lat_max)
+print(x_min)
+print(y_min)
+print(x_max)
+print(y_max)
 
-
-
-xmin =  str(bbox_s[0])
-ymin =  str(bbox_s[1])
-xmax = str(bbox_s[2])
-ymax =  str(bbox_s[3])
-
-
-
-#19.103132247924805, 64.041618347167969, 19.898113250732422,64.403816223144531
 #---
 #download_unzipped_path = os.path.join(os.getcwd(), 'unzipped')
 download_unzipped_path = os.path.join(os.getcwd(), 'data')
-#download_unzipped_path = os.path.join(os.getcwd(), 'unzipped3')  #unzipped
+
 listfiles =[]
 for item in os.listdir(download_unzipped_path):
     file_names = os.path.join(download_unzipped_path, item)
@@ -264,26 +193,37 @@ nir = [bands(file,res='10m')[4]  for file in listfiles]
 redlist = red
 nirlist = nir
 
-#time_var = xr.Variable('time', paths_to_datetimeindex(redlist))
-#print(time_var)
 
-window10= rasterio.windows.Window(0,0, 1080,1080)
-window60= rasterio.windows.Window(0,0, 1080/6,1080/6)
+#window
+#window10= rasterio.windows.Window(0,0, 1080,1080)
+#window60= rasterio.windows.Window(0,0, 1080/6,1080/6)
 
 
 nir_da_gran = xr.concat([xr.open_rasterio(i,chunks={'x':512, 'y':512}) for i in nirlist],
 #nir_da_gran = xr.concat([xr.open_rasterio(i) for i in nirlist],
                         dim=time_var)  #[:,:,0:20,0:20]
 
-nir_ds = nir_da_gran.rio.isel_window(window10).to_dataset('band')
-nir_ds = nir_ds.rename({1: 'nir'})#[:,:,0:500,0:500]
+
+mask_lon = (nir_da_gran.x >= x_max) & (nir_da_gran.x <= x_min)   #   429770.327 4666780.373
+mask_lat = (nir_da_gran.y >= y_max) & (nir_da_gran.y <= y_min)
+
+nir_da = nir_da_gran.where(mask_lon & mask_lat, drop=True)
+
+#nir_ds = nir_da_gran.rio.isel_window(window10).to_dataset('band')
+nir_ds = nir_da.to_dataset('band')
+nir_ds = nir_ds.rename({1: 'nir'})
 nir_ds = nir_ds.astype('int16')
 print(nir_ds)
 
 red_da_gran = xr.concat([xr.open_rasterio(i,chunks={'x':512, 'y':512}) for i in redlist],
 #red_da_gran = xr.concat([xr.open_rasterio(i) for i in redlist],
-                        dim=time_var)  #[:,:,0:20,0:20]
-red_ds = red_da_gran.rio.isel_window(window10).to_dataset('band')
+                        dim=time_var)
+
+#mask_lon = (red_da_gran.x >= x_max) & (red_da_gran.x <= x_min)
+#mask_lat = (red_da_gran.y >= y_max) & (red_da_gran.y <= y_min)
+#red_da = red_da_gran.where(mask_lon & mask_lat, drop=True)
+
+#red_ds = red_da_gran.rio.isel_window(window10).to_dataset('band')
 red_ds = red_ds.rename({1: 'red'})#[:,:,0:500,0:500]
 red_ds = red_ds.astype('int16')
 print(red_ds)
@@ -291,7 +231,7 @@ print(red_ds)
 
 scl = []
 for file in listfiles:
-    scl_list = sclbands(file)[0]     #dp2.sclbands(file)[0]  sclbands(file)[1]
+    scl_list = sclbands(file)[1]     #dp2.sclbands(file)[0]  sclbands(file)[1]
     print(scl_list)
     scl.append(scl_list)
 
@@ -326,9 +266,7 @@ good_data = scl.where((scl == 4) | (scl == 5) | (scl == 6))
 ndvi_no_cloud = ndvi.where(good_data>=5)  #ndvi
 #ndvi_no_cloud
 
-
-##write_cog(geo_im=ndvi.isel(time=0), fname=filename)
-
+##Plot _output
 #ndvi.isel(time=0).rio.to_raster("ndvi_0.tif")
 ndvi.rio.to_raster('ndvi_output.tif', dtype="float32")
 
@@ -336,6 +274,10 @@ mask = ndvi.isnull()
 #mask
 ndvi_cl = ndvi.where(~mask, other=0)
 print(ndvi_cl)
+
+#Plot output
+ndvi.rio.to_raster('ndvi_cl_output.tif', dtype="float32")
+
 
 # vPOS = Value at peak of season:
 ndvi_cl.max("time").values
